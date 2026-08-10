a03711fa76e270e1d8a7043d97976ef964fefb14
abheetha <abeetha.dilushan@gmail.com>
2026-08-10 13:58:09 +0530

    feat(view): implement log in screen UI

--- src/dao/impl/TreatmentDAOImpl.java
+++ src/dao/impl/TreatmentDAOImpl.java
@@ -4,10 +4,33 @@
  */
 package dao.impl;
 
+import dao.TreatmentDAO;
+import model.Treatment;
+
 /**
  *
  * @author ASUS
  */
-public class TreatmentDAOImpl {
+public class TreatmentDAOImpl implements TreatmentDAO{
+
+    @Override
+    public Treatment createTreatment(Treatment treatment) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Treatment updateTreatment(Treatment treatment) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Treatment deleteTreatment(Treatment treatment) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Treatment findByTreatmentName(String treatment) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
